12053408f2ba74edbcfd7f95f4b145c5c0b1008f
fars90 <andreraposeirofaria@gmail.com>
2025-07-01 13:15:37 +0100

    Update main.py

--- main.py
+++ main.py
@@ -3,14 +3,23 @@ import time
 
 def calculadora(num1: float, num2: float, operador: str) -> float:
     """
-    Usar nan como valor inicial é uma boa prática. 
-    Se o operador fornecido não corresponder a nenhuma das opções válidas (+, -, etc.), a função retornará nan, 
-    sinalizando que o cálculo não pôde ser realizado.
+    Função para realizar operações matemáticas básicas.
+    Retorna NaN se o operador for inválido.
     """
     result = float("nan")
     if operador == '+':
         result = num1 + num2
-
+    elif operador == '-':
+        result = num1 - num2
+    elif operador == '*':
+        result = num1 * num2
+    elif operador == '/':
+        if num2 != 0:
+            result = num1 / num2
+        else:
+            raise ZeroDivisionError
+    elif operador == '^':
+        result = num1 ** num2
     return result
 
 
